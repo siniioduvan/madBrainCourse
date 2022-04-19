32d15f2f0d6526e112113291079535f8ffe11652
siniioduvan <sinii.oduvan@gmail.com>
2022-04-19 18:11:15 +0400

    19.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,122 @@
-void main() {}
+import 'package:flutter/material.dart';
 
 abstract class AbstractFilm {
-  String? id;
-  String? title;
-  String? picture;
-  double? voteAverage;
-  String? releaseDate;
-  String? description;
-  late String language;
+  const AbstractFilm(this.id, this.title, this.picture, this.voteAverage,
+      this.releaseDate, this.description, this.language);
 
+  final String id;
+  final String title;
+  final String picture;
+  final double voteAverage;
+  final String releaseDate;
+  final String description;
+  final String language;
+
+  void aboutFilm();
 }
 
-class Film extends AbstractFilm with ConvertLanguage {
+enum Language { russian, english, france, deutsch }
 
-}
-
-enum Language { russian, english }
-
-mixin ConvertLanguage implements AbstractFilm {
-
-  late Language lan;
-
-  Language convertStringToEnum() {
-    if (language == 'russian') {
-      lan = Language.russian;
-    } else if (language == 'english') {
-      lan = Language.english;
+extension LanguageExtension on Language {
+  String toPrettyString() {
+    switch (this) {
+      case Language.deutsch:
+        return 'Немецкий';
+      case Language.english:
+        return 'Английский';
+      case Language.france:
+        return 'Французский';
+      case Language.russian:
+        return 'Русский';
     }
-    return lan;
   }
 }
 
-extension LanguageEnum on Language {
+class Film extends AbstractFilm with languageConvert{
+  const Film({
+    required String id,
+    required String title,
+    required String picture,
+    required double voteAverage,
+    required String releaseDate,
+    required String description,
+    required String language,
+  }) : super(
+      id,
+      title,
+      picture,
+      voteAverage,
+      releaseDate,
+      description,
+      language);
 
-  String toPrettyString(Language lan) {
-    late String lang;
-    if (lan == Language.russian) {
-      lang = "russian";
-    } else if (lan == Language.english) {
-      lang = "english";
-    }
-    return lang;
+  @override
+  void aboutFilm() {
+    print('Фильм: $title, описание: $description, рейтинг: $voteAverage');
   }
+
+}
+
+mixin languageConvert {
+  Language? getLanguage (String language) {
+    switch (language) {
+      case 'russian':
+        return Language.russian;
+      case 'english':
+        return Language.english;
+      case 'france':
+        return Language.france;
+      case 'deutsch':
+        return Language.deutsch;
+    }
+  }
+}
+
+List<Film> _getFilm() {
+  return [
+    const Film(
+        id: "01",
+        title: "Зеленая миля",
+        picture:
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/4057c4b8-8208-4a04-b169-26b0661453e3/600x900',
+        voteAverage: 9.1,
+        releaseDate: '6 декабря 1999',
+        description:
+            'Пол Эджкомб — начальник блока смертников в тюрьме «Холодная гора», каждый из узников которого однажды проходит «зеленую милю» по пути к месту казни. Пол повидал много заключённых и надзирателей за время работы. Однако гигант Джон Коффи, обвинённый в страшном преступлении, стал одним из самых необычных обитателей блока.,',
+        language: 'russian'),
+    const Film(
+        id: '02',
+        title: 'Побег из Шоушенка',
+        picture:
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/0b76b2a2-d1c7-4f04-a284-80ff7bb709a4/600x900',
+        voteAverage: 9.1,
+        releaseDate: '10 сентября 1994',
+        description:
+            'Бухгалтер Энди Дюфрейн обвинён в убийстве собственной жены и её любовника. Оказавшись в тюрьме под названием Шоушенк, он сталкивается с жестокостью и беззаконием,',
+        language: 'russian'),
+    const Film(
+        id: '03',
+        title: 'Список Шиндлера',
+        picture:
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1773646/b327ada7-d790-49ae-8b24-374497a0980c/600x900',
+        voteAverage: 8.8,
+        releaseDate: '30 ноября 1993',
+        description:
+            'История немецкого промышленника, спасшего тысячи жизней во время Холокоста. Драма Стивена Спилберга',
+        language: 'russian'),
+    const Film(
+        id: '04',
+        title: 'Властелин колец',
+        picture:
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/e410c71f-baa1-4fe5-bb29-aedb4662f49b/600x900',
+        voteAverage: 8.6,
+        releaseDate: '01 декабря 2003',
+        description:
+            'Повелитель сил тьмы Саурон направляет свою бесчисленную армию под стены Минас-Тирита, крепости Последней Надежды.',
+        language: 'russian')
+  ];
+}
+
+void main() {
+  List<Film> film = _getFilm();
 }
