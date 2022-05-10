fa2bc95aab6bb72dbcf539dbec5fe7c601d6708b
siniioduvan <sinii.oduvan@gmail.com>
2022-05-10 19:02:23 +0400

    10.6

---
+++ lib/app/widgets/film_grid_card.dart
@@ -0,0 +1,53 @@
+
+import 'package:film/app/feature/home/widgets/image_network.dart';
+import 'package:flutter/material.dart';
+
+class FilmGridCard extends StatelessWidget {
+  const FilmGridCard({
+    required this.id,
+    required this.title,
+    required this.picture,
+    required this.voteAverage,
+    Key? key,
+  }) : super(key: key);
+
+  factory FilmGridCard.fromFilm({
+    required FilmGridCard model,
+    Key? key,
+  }) {
+    return FilmGridCard(
+      id: model.id,
+      title: model.title,
+      picture: model.picture,
+      voteAverage: model.voteAverage,
+      key: key,
+    );
+  }
+
+  final int id;
+  final String title;
+  final String picture;
+  final double voteAverage;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const <BoxShadow>[
+          BoxShadow(
+            offset: Offset(1, 2),
+            blurRadius: 5,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: ImageNetwork(
+          picture,
+        ),
+      ),
+    );
+  }
+}
